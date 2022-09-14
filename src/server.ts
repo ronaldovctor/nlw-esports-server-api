9c17b81d6c6370e3cb77b2bd9b3dbe161a61f90a
Ronaldo <ronaldovctor@outlook.com>
2022-09-13 21:45:53 -0300

    Getting games with ads

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,15 @@ const app = express()
 const prisma = new PrismaClient()
 
 app.get('/games', async (request: Request, response: Response) => {
-	const games = await prisma.game.findMany()
+	const games = await prisma.game.findMany({
+		include: {
+			_count: {
+				select: {
+					ads: true,
+				},
+			},
+		},
+	})
 	return response.json(games)
 })
 
